b59cddd74fdb88915165ed4e9862c3139ce87e9a
Syohei YOSHIDA <syohex@gmail.com>
2014-11-11 08:45:50 +0900

    Specify minimum Emacs version

--- sgit.el
+++ sgit.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2014 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
+;; Package-Requires: ((emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
